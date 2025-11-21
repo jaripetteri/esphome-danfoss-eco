47032b8c59f2df20b2283ba1aa76142571c54a03
jaripetteri <jaripetteri@gmail.com>
2025-11-21 22:32:44 +0200

    Revert "add xxtea-iot-crypt.h"
    This reverts commit cd0110b2f08e1424381087d0745a043b46f02484.

--- components/danfoss_eco/xxtea.h
+++ components/danfoss_eco/xxtea.h
@@ -1,17 +1,15 @@
 #pragma once
 
-#include "xxtea-iot-crypt.h"
+#include <xxtea-lib.h>
 
 // key Size is always fixed
 #define MAX_XXTEA_KEY8 16
 // 32 Bit
 #define MAX_XXTEA_KEY32 4
-// Status codes
+// DWORD Size of Data Buffer
+#define MAX_XXTEA_DATA32 (UINT32CALCBYTE(MAX_XXTEA_DATA8))
+
 #define XXTEA_STATUS_NOT_INITIALIZED -1
-#define XXTEA_STATUS_SUCCESS 0
-#define XXTEA_STATUS_GENERAL_ERROR 1
-#define XXTEA_STATUS_PARAMETER_ERROR 2
-#define XXTEA_STATUS_SIZE_ERROR 3
 
 class Xxtea
 {
@@ -21,11 +19,12 @@ public:
     int set_key(uint8_t *key, size_t len);
 
     int encrypt(uint8_t *data, size_t len, uint8_t *buf, size_t *maxlen);
-    int decrypt(uint8_t *data, size_t len, uint8_t *buf, size_t *outlen);
+    int decrypt(uint8_t *data, size_t len);
 
     int status() { return this->status_; }
 
 private:
     int status_;
-    uint8_t xxtea_key[MAX_XXTEA_KEY8];
+    uint32_t xxtea_data[MAX_XXTEA_DATA32];
+    uint32_t xxtea_key[MAX_XXTEA_KEY32];
 };
